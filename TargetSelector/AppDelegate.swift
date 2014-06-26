fae527ae37fecd6147a258e1afab709e0fe7c0db
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-06-26 21:43:18 +0200

    Initial commit.

---
+++ TargetSelector/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TargetSelector
+//
+//  Created by Benjamin Horsleben on 26/06/14.
+//  Copyright (c) 2014 ReturnTool ApS. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+	@IBOutlet var window: NSWindow
+
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+}
---
+++ TargetSelector/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  TargetSelector
+//
+//  Created by Benjamin Horsleben on 26/06/14.
+//  Copyright (c) 2014 ReturnTool ApS. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
