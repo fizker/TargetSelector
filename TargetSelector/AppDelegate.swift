f43e0706e68ea820d7bccaa51af1fe7d5780dc5b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-06-28 15:24:36 +0200

    Current target is now properly updated when the user clicks around.

--- TargetSelector/AppDelegate.swift
+++ TargetSelector/AppDelegate.swift
@@ -11,16 +11,36 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 	@IBOutlet var window: NSWindow
 
+	let targetsHelper = Targets(projectPath: "/Users/benjamin/Development/poteo/iphone")
+
 	func applicationDidFinishLaunching(aNotification: NSNotification?) {
 		// Insert code here to initialize your application
+		var i = 0
+		let currentTarget = targetsHelper.getCurrentTarget()
+		for target in targets {
+			if target.name == currentTarget {
+				println("Found current app: \(target.name)")
+				break
+			}
+			i++
+		}
+		selectedIndexes = NSIndexSet(index: i)
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification?) {
 		// Insert code here to tear down your application
 	}
 
+	var selectedIndexes: NSIndexSet = NSIndexSet() {
+		didSet {
+			let index = selectedIndexes.firstIndex
+			let target = targetsHelper.loadTargets()[index]
+			targetsHelper.setCurrentTarget(target)
+		}
+	}
+
 	var targets : TargetBridge[] {
-		return loadTargets("/Users/benjamin/Development/poteo/iphone").map({
+		return targetsHelper.loadTargets().map({
 			return TargetBridge.newWithTarget($0)
 		})
 	}
--- TargetSelector/Target.swift
+++ TargetSelector/Target.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Cocoa
+import JavaScriptCore
 
 @objc
 class Target : Printable {
@@ -37,22 +38,61 @@ class Target : Printable {
 	}
 }
 
-func loadTargets(projectPath:String) -> Target[] {
-	let fileManager = NSFileManager.defaultManager()
-	var error : NSError?
-
-	if let contents = fileManager.contentsOfDirectoryAtURL(
-		NSURL(fileURLWithPath: projectPath + "/products"),
-		includingPropertiesForKeys: [NSURLIsDirectoryKey],
-		options: .SkipsHiddenFiles,
-		error: &error) as? NSURL[]
-	{
-		return contents.map({
-			Target(url: $0)
-		})
+class Targets {
+	let projectPath: String
+	init(projectPath:String) {
+		self.projectPath = projectPath
 	}
 
-	println("Got error: \(error?.localizedDescription)")
+	func loadTargets() -> Target[] {
+		let fileManager = NSFileManager.defaultManager()
+		var error : NSError?
 
-	return []
+		if let contents = fileManager.contentsOfDirectoryAtURL(
+			NSURL(fileURLWithPath: projectPath + "/products"),
+			includingPropertiesForKeys: [NSURLIsDirectoryKey],
+			options: .SkipsHiddenFiles,
+			error: &error) as? NSURL[]
+		{
+			return contents.map({
+				Target(url: $0)
+			})
+		}
+
+		println("Got error: \(error?.localizedDescription)")
+
+		return []
+	}
+
+	func getCurrentTarget() -> String {
+		let fileManager = NSFileManager.defaultManager()
+		var error : NSError?
+
+		if let content = String.stringWithContentsOfFile(projectPath + "/Target.xcconfig", encoding: NSUTF8StringEncoding, error: &error) {
+			let context = JSContext()
+			let result = context.evaluateScript("'\(content)'.match(/CURRENT_TARGET_NAME *= *(.+)/)[1]")
+			return result.toString()
+		}
+
+		return ""
+	}
+	func setCurrentTarget(target:String) {
+		let errorPipe = NSPipe()
+		let task = NSTask()
+		task.launchPath = "/usr/local/bin/node"
+		task.arguments = [projectPath + "/scripts/set-current-target.js", target]
+		task.standardError = errorPipe
+		task.launch()
+		task.waitUntilExit()
+
+		let file = errorPipe.fileHandleForReading
+		let data = file.readDataToEndOfFile()
+		if data.length > 0 {
+			let contents = NSString(data: data, encoding: NSUTF8StringEncoding)
+			println("Error with running set-current-target: \(contents)")
+		}
+	}
+	func setCurrentTarget(target:Target) {
+		setCurrentTarget(target.name)
+	}
 }
